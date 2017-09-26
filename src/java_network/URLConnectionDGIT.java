9d74ffeae84d9c25046526a9ea0eb9e0795ff41b
S_YoonPyo <ypyo09@naver.com>
2017-09-26 15:27:32 +0900

    urlconnectiondgit.java 추가

---
+++ src/java_network/URLConnectionDGIT.java
@@ -0,0 +1,38 @@
+package java_network;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class URLConnectionDGIT {
+
+	public static void main(String[] args) {
+		try {
+			URL dgit = new URL("http://dgit.or.kr/login.php");
+			URLConnection urlConnection = dgit.openConnection();
+			urlConnection.setDoOutput(true);
+			
+			OutputStreamWriter osw = new OutputStreamWriter(urlConnection.getOutputStream());
+			osw.write("user_id=ypyo09&userpasswd=unrock147");
+			//http://dgit.or.kr/login.php?user_id=ypyo09&userpasswd=
+			osw.close();
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+			String line;
+			while((line=br.readLine()) != null){
+				System.out.println(line);
+			}
+			br.close();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
